016e76e3911681d72984d2a9576bfbd624b27ad9
Mark Spence <mosspence@gmail.com>
2014-06-10 00:38:31 -0400

    added some basic controller code which requires editing

---
+++ appsrc/moss/musicapp/controllers/searchControllerProvider.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace moss\musicapp\controllers;
+
+/**
+ * Description of searchController
+ *
+ * @author mosspence
+ * search controller
+ */
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+ 
+class searchControllerProvider implements ControllerProviderInterface
+{
+    public function connect(Application $app)
+    {
+        // creates a new controller based on the default route
+        $controllers = $app['controllers_factory'];
+        
+        $controllers->$app->get('/{string}', function ($string){
+           
+            $sch = new moss\musicapp\songSearch($string);
+            $songs = $sch->search();
+
+            $returnVal = json_encode(array("offset"=>$sch->__get('offset'), 
+                    "numRows"=> count($songs), 
+                    "numResults"=>$sch->__get('numResults'), 
+                    "limit"=>$sch->__get('limit'), "page"=>$sch->__get('page'), 
+                    "query"=>$string, "cleaned_query"=>$sch->__get('searchString'), 
+                    "songs"=> $songs));
+            //json_encode($returnVal);
+
+            return new \Symfony\Component\HttpFoundation\Response($returnVal, 200);
+        });
+
+        $controllers->$app->get('/{string}/{page}', function (Silex\Application $app, $string) {
+
+            $sch = new moss\musicapp\songSearch($string, '', '', $page);
+            $songs = $sch->search();
+
+            $returnVal = json_encode(array("offset"=>$sch->__get('offset'), 
+                    "numRows"=> count($songs), 
+                    "numResults"=>$sch->__get('numResults'), 
+                    "limit"=>$sch->__get('limit'), "page"=>$sch->__get('page'), 
+                    "query"=>$string, "cleaned_query"=>$sch->__get('searchString'), 
+                    "songs"=> $songs));
+            //json_encode($returnVal);
+
+            return new \Symfony\Component\HttpFoundation\Response($returnVal, 200);
+        })->value('page', 1)->assert('page', '\d+');
+        // */
+        return $controllers;
+    }
+}
+
+
+
+/*
+{
+    public function indexAction(Application $app, $string, $page)
+    {
+        $sch = new moss\musicapp\songSearch($string, '', '', $page);
+        $songs = $sch->search();
+
+        $returnVal = json_encode(array("offset"=>$sch->__get('offset'), 
+                "numRows"=> count($songs), 
+                "numResults"=>$sch->__get('numResults'), 
+                "limit"=>$sch->__get('limit'), "page"=>$sch->__get('page'), 
+                "query"=>$string, "cleaned_query"=>$sch->__get('searchString'), 
+                "songs"=> $songs));
+        
+        return new Response($returnVal, 200);
+    }
+    
+    public function toolAction(Application $app, $key, $bpm, $page, $kmove, $bmove)
+    {
+        $sch = new moss\musicapp\songSearch('', $bpm, $key, $page, $kmove, $bmove);
+        $songs = $sch->search();
+
+        $returnVal = json_encode(array("offset"=>$sch->__get('offset'), 
+                "numRows"=> count($songs), 
+                "numResults"=>$sch->__get('numResults'), 
+                "limit"=>$sch->__get('limit'), "page"=>$sch->__get('page'),
+                "bpm"=>$sch->__get('bpmString'), "key"=>$sch->__get('keySearch'),
+                "bpmMovement"=>$sch->__get('bpmMovement'), "keyMovement"=>$sch->__get('keyMovement'), 
+                "songs"=> $songs));
+        
+        return new Response($returnVal, 200);
+    }
+}
+// */
